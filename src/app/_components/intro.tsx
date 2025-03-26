e320cb7a71f19e859365a9d813dc555ff8b0dc4f
Itsfoss0 <akhonyaebenezer@gmail.com>
2025-03-26 03:33:10 +0300

    [chore]: personalize blog sections and general feel
    Summary:
    Add author avatar (Ebenezer's) to Include in blogs and
    Modify the components to fit my style. Also add intros
    for some base blogs and writeups to start working on

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,20 +1,19 @@
-import { CMS_NAME } from "@/lib/constants";
 
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-6 mb-6 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Itsfoss Reports.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        Writeups about incidents and new developments in my{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://github.com/Itsfoss0/withk8s"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
+          Home Lab
         </a>{" "}
-        and {CMS_NAME}.
+        and postmoterms for outages.
       </h4>
     </section>
   );
